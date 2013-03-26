e65e4b0a65e04cb60ad60df0ae48a1b9a5068bc1
Stephen Tredger <stredger@gmail.com>
2013-03-26 13:45:06 -0700

    added partition function

--- python/orchive.py
+++ python/orchive.py
@@ -42,34 +42,37 @@ def read_csv(fname):
 
 def partition_lists(h, a, c, n=10):
 
+    id_col = 1
+
     train = {'heads':[], 'attrs':[], 'class':[]}
     classify = {'heads':[], 'attrs':[], 'class':[]}
 
-    if len(l) < n:
-        print "list length larger than fold! exiting"
+    if len(h) < n:
+        print "list length smaller than fold! exiting"
         sys.exit()
 
-    for i in xrange(len(l)):
-        if i % n:
-            classify.append(l[i])
+    for i in xrange(len(h)):
+        if int(h[i][id_col]) % n:
+            classify['heads'].append(h[i])
+            classify['attrs'].append(a[i])
+            classify['class'].append(c[i])
         else:
-            train.append(l[i])
+            train['heads'].append(h[i])
+            train['attrs'].append(a[i])
+            train['class'].append(c[i])
 
     return train, classify
-
-
-
 
 
 h, a, c = read_csv('orchive_svm_25-03-2013.csv')
 
 
-ht, hc = partition_list(h)
-at, ac = partition_list(a)
-ct, cc = partition_list(c)
+training, classify = partition_lists(h, a, c)
+#print len(training['heads']), len(classify['heads'])
 
-m = svm_train(ct, at)
-resutls = svm_predict([cc[0]],[ac[0]], m, '-b 0')
+
+m = svm_train(training['class'], training['attrs'])
+results = svm_predict(classify['class'],classify['attrs'], m)
 
 print results
 
